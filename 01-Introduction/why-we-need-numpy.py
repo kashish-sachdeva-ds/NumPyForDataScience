5777842087bf08587478e921bd2d4ecd9286e669
Kashish Sachdeva <kashish.sachdeva.ds@gmail.com>
2025-06-15 20:36:49 +0530

    why we need numpy

--- 01-Introduction/why-we-need-numpy.py
+++ 01-Introduction/why-we-need-numpy.py
@@ -9,7 +9,7 @@ for temp in temperatures:
 average = total / len(temperatures)
 print(average)
 
-# Output: 29.6 approx
+# Output: 29.287 approx
 
 
 '''Note:
@@ -21,3 +21,18 @@ That's where NumPy helps:
 NumPy allows fast, optimized, and vectorized operations on large datasets.
 Instead of using loops, we can use numpy arrays and functions like numpy.mean().
 This makes code cleaner, faster, and more suitable for data science and ML tasks.'''
+
+
+
+# Example: Calculating average temperature using NumPy
+
+import numpy as np
+
+temperatures_np = np.array([32.5, 31.0, 30.5, 29.0, 28.5, 27.0, 26.5])
+average_np = np.mean(temperatures_np)
+print(average_np)
+
+# Output: 29.287 approx
+
+'''it is fast, memory efficient, 
+and optimized for numerical operations on large data.'''
